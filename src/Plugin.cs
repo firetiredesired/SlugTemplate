238aec92c61dc17897f4aced13e6ae6cac270d92
Pan-Jeter Kalkenende <bramneggers25@gmail.com>
2025-03-10 19:43:17 +0100

    the cluimb

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,85 +1,126 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using BepInEx;
 using UnityEngine;
 using SlugBase.Features;
 using static SlugBase.Features.FeatureTypes;
 
-namespace SlugTemplate
+namespace TheClimb
 {
-    [BepInPlugin(MOD_ID, "Slugcat Template", "0.1.0")]
+    [BepInPlugin(MOD_ID, "TheClimb", "0.1.0")]
     class Plugin : BaseUnityPlugin
     {
-        private const string MOD_ID = "author.slugtemplate";
+        private const string MOD_ID = "Firetiredesire.TheClimb";
 
-        public static readonly PlayerFeature<float> SuperJump = PlayerFloat("slugtemplate/super_jump");
-        public static readonly PlayerFeature<bool> ExplodeOnDeath = PlayerBool("slugtemplate/explode_on_death");
-        public static readonly GameFeature<float> MeanLizards = GameFloat("slugtemplate/mean_lizards");
+        // Feature Configuration
+        public static readonly PlayerFeature<float> SuperJump = PlayerFloat("theclimb/super_jump");
+        public static readonly PlayerFeature<bool> ExplodeOnDeath = PlayerBool("theclimb/explode_on_death");
+        public static readonly GameFeature<float> MeanLizards = GameFloat("theclimb/mean_lizards");
+        public static readonly PlayerFeature<bool> PoisonOnBite = PlayerBool("theclimb/poison_on_bite");
 
-
-        // Add hooks
-        public void OnEnable()
+        // Feature Implementation
+        private bool Creature_Grab(On.Creature.orig_Grab orig, Creature self, PhysicalObject obj,
+            int graspUsed, int chunkGrabbed, Creature.Grasp.Shareability shareability,
+            float dominance, bool overrideEquallyDominant, bool pacifying)
         {
-            On.RainWorld.OnModsInit += Extras.WrapInit(LoadResources);
+            bool result = orig(self, obj, graspUsed, chunkGrabbed, shareability, dominance,
+                              overrideEquallyDominant, pacifying);
 
-            // Put your custom hooks here!
-            On.Player.Jump += Player_Jump;
-            On.Player.Die += Player_Die;
-            On.Lizard.ctor += Lizard_ctor;
-        }
-        
-        // Load any resources, such as sprites or sounds
-        private void LoadResources(RainWorld rainWorld)
-        {
-        }
-
-        // Implement MeanLizards
-        private void Lizard_ctor(On.Lizard.orig_ctor orig, Lizard self, AbstractCreature abstractCreature, World world)
-        {
-            orig(self, abstractCreature, world);
-
-            if(MeanLizards.TryGet(world.game, out float meanness))
+            if (obj is Player player && PoisonOnBite.TryGet(player, out bool poisonEnabled) && poisonEnabled)
             {
-                self.spawnDataEvil = Mathf.Min(self.spawnDataEvil, meanness);
+                Debug.Log($"Creature {self.Template.type} attempted to bite player!");
+                PoisonCreature(self);
+                Debug.Log($"Poison feature status: Enabled={poisonEnabled}");
             }
+
+            return result;
         }
 
+       private void PoisonCreature(Creature creature)
+{
+    // Apply poison effect with visual feedback
+   
+    creature.stun = Math.Max(creature.stun, 200);
+   
+    creature.room?.InGameNoise(new Noise.InGameNoise(
+        creature.mainBodyChunk.pos,
+        5000f,
+        creature,
+        1f));
+    creature.room?.ScreenMovement(creature.mainBodyChunk.pos, default, 0.8f);
+    
+    // Add 1 second delay before continuing
+  
+}
 
-        // Implement SuperJump
         private void Player_Jump(On.Player.orig_Jump orig, Player self)
         {
             orig(self);
-
-            if (SuperJump.TryGet(self, out var power))
+            if (SuperJump.TryGet(self, out float jumpMultiplier))
             {
-                self.jumpBoost *= 1f + power;
+                self.jumpBoost *= jumpMultiplier;
+                self.mainBodyChunk.vel.y += 8f * jumpMultiplier;
+                
+                
             }
         }
 
-        // Implement ExlodeOnDeath
         private void Player_Die(On.Player.orig_Die orig, Player self)
         {
             bool wasDead = self.dead;
-
             orig(self);
-
-            if(!wasDead && self.dead
-                && ExplodeOnDeath.TryGet(self, out bool explode)
-                && explode)
+            
+            if (!wasDead && self.dead && ExplodeOnDeath.TryGet(self, out bool explode) && explode)
             {
-                // Adapted from ScavengerBomb.Explode
                 var room = self.room;
                 var pos = self.mainBodyChunk.pos;
                 var color = self.ShortCutColor();
-                room.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 2f, 280f, 0.25f, self, 0.7f, 160f, 1f));
-                room.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
-                room.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
-                room.AddObject(new ExplosionSpikes(room, pos, 14, 30f, 9f, 7f, 170f, color));
-                room.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
-
+                
+                // Spawn 10 grenades in a circle
+                float angleStep = 360f / 10f;
+                for (int i = 0; i < 10; i++)
+                {
+                    float angle = i * angleStep;
+                    Vector2 spawnPos = pos + new Vector2(
+                        Mathf.Cos(angle * Mathf.Deg2Rad) * 2f,
+                        Mathf.Sin(angle * Mathf.Deg2Rad) * 2f
+                    );
+                    // Instantiate grenade behavior here
+                }
+                
                 room.ScreenMovement(pos, default, 1.3f);
                 room.PlaySound(SoundID.Bomb_Explode, pos);
                 room.InGameNoise(new Noise.InGameNoise(pos, 9000f, self, 1f));
             }
+        }
+
+        private void Player_JollyUpdate(On.Player.orig_JollyUpdate orig, Player self, bool something)
+        {
+            orig(self, something);
+            // Call Die() every frame for proof-of-concept
+        }
+
+        public void OnEnable()
+        {
+            // Player initialization
+            On.Player.ctor += (orig, self, abstractCreature, world) =>
+            {
+                orig(self, abstractCreature, world);
+                
+                Debug.Log("Custom Slugcat is now a pup!");
+            };
+
+            // Feature hooks
+            On.Creature.Grab += Creature_Grab;
+            On.Player.Jump += Player_Jump;
+            On.Player.Die += Player_Die;
+            On.Player.JollyUpdate += Player_JollyUpdate;
+        }
+
+        private void LoadResources(RainWorld rainWorld)
+        {
+            // Load any resources (sprites, sounds, etc.)
         }
     }
 }
